aab97976539c6f44b6b266bdeb5d29031f3d39c3
srcspider <source.spider@gmail.com>
2012-11-05 16:10:50 +0200

    updated honeypot

--- +App/honeypot.php
+++ +App/honeypot.php
@@ -6,5 +6,5 @@
 // HowTo: order honeypot -n 'mjolnir\testing'
 
 class AnotherExampleClass extends \mjolnir\testing\AnotherExampleClass { /** @return \mjolnir\testing\AnotherExampleClass */ static function instance() { return parent::instance(); } }
-class ExampleClass extends \mjolnir\testing\ExampleClass { /** @return \mjolnir\testing\ExampleClass */ static function instance() { return parent::instance(); } }
 class HiddenExampleClass extends \mjolnir\testing\HiddenExampleClass { /** @return \mjolnir\testing\HiddenExampleClass */ static function instance() { return parent::instance(); } }
+class SimpleExampleClass extends \mjolnir\testing\SimpleExampleClass { /** @return \mjolnir\testing\SimpleExampleClass */ static function instance() { return parent::instance(); } }
