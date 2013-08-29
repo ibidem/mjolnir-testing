8b47dd6c928fcd037f62f9e0bdaae47fe5bd8882
srcspider <source.spider@gmail.com>
2013-08-29 10:46:30 +0300

    overwrite hooks

---
+++ PHPUnit/Extensions/Database/TestCase.php
@@ -0,0 +1,14 @@
+<?php namespace mjolnir\testing;
+
+/**
+ * @package    mjolnir
+ * @category   PHPUnit
+ * @author     Ibidem Team
+ * @copyright  (c) 2013, Ibidem Team
+ * @license    https://github.com/ibidem/ibidem/blob/master/LICENSE.md
+ */
+class PHPUnit_Extensions_Database_TestCase extends \PHPUnit_Extensions_Database_TestCase
+{
+	// overwrite hook
+
+} # class
---
+++ PHPUnit/Framework/TestCase.php
@@ -0,0 +1,14 @@
+<?php namespace mjolnir\testing;
+
+/**
+ * @package    mjolnir
+ * @category   PHPUnit
+ * @author     Ibidem Team
+ * @copyright  (c) 2013, Ibidem Team
+ * @license    https://github.com/ibidem/ibidem/blob/master/LICENSE.md
+ */
+class PHPUnit_Framework_TestCase extends \PHPUnit_Framework_TestCase
+{
+	// overwrite hook
+
+} # class
