91b9f2e47c9caf8239568544d91adeff4e1f4439
srcspider <source.spider@gmail.com>
2013-08-29 10:58:06 +0300

    stub

--- PHPUnit/Extensions/Database/TestCase.php
+++ PHPUnit/Extensions/Database/TestCase.php
@@ -9,6 +9,19 @@
  */
 class PHPUnit_Extensions_Database_TestCase extends \PHPUnit_Extensions_Database_TestCase
 {
-	// overwrite hook
+	static private $pdo = null;
+	private $conn = null;
+
+	function getConnection()
+    {
+        if ($this->conn === null) {
+            if (self::$pdo == null) {
+                self::$pdo = new PDO('sqlite::memory:');
+            }
+            $this->conn = $this->createDefaultDBConnection(self::$pdo, ':memory:');
+        }
+
+        return $this->conn;
+    }
 
 } # class
