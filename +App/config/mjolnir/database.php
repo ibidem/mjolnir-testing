da25654ce2caecb1f6484c6d4b340fe4a5580ada
srcspider <source.spider@gmail.com>
2014-03-17 16:14:34 +0200

    removed dialects

--- +App/config/mjolnir/database.php
+++ +App/config/mjolnir/database.php
@@ -28,21 +28,6 @@
 						'charset'      => 'utf8',
 						'caching'      => false,
 						'profiling'    => true,
-						/**
-						 * Dialect options. The default dialect is what will be assumed
-						 * for your statements when no language is specified. The target
-						 * dialect is the language your database engine understands. If
-						 * both are the same then your statements will be prepared as-is.
-						 * If the default dialect doesn't match the target dialect then
-						 * the statement will be
-						 * translated.
-						 *
-						 * If you wish to have your statements in configuration files, you may
-						 * set the default dialect to something like 'src', and then you would
-						 * just pass keys as statements.
-						 */
-						'dialect_default' => 'mysql',
-						'dialect_target' => 'mysql',
 					),
 			),
 	);
